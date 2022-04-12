2e165e36d7f1b8e2ff5b8355a6d869747172c665
Brandt-C <brandt.carlson11@gmail.com>
2022-04-12 18:58:24 -0500

    working for login on back

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, jsonify, request
 from flask_cors import cross_origin
 from app.models import Post, User, db
 
@@ -10,13 +10,23 @@ api = Blueprint('api', __name__, template_folder='apitemps', url_prefix='/api')
 @api.route('/reg', methods=['POST'])
 def register():
     data = request.get_json()
-    print(data)
+    # print(data)
     newuser = User(username=data['username'], email=data['email'], first_name=data['first_name'], last_name=data['last_name'], userpass=data['userpass'] )  # Have to figure out this part! current err=
     # TypeError: __init__() missing 3 required positional arguments: 'email', 'first_name', and 'last_name'
     db.session.add(newuser)
     db.session.commit()
     print('New user reg!')
     return 'Registration success!', 201
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user and user.userpass == data['userpass']:
+        print(type(user), user)
+        return jsonify({'userid': user.id, 'username': user.username, 'email': user.email}), 200 #Continue on here!  Let's see if we can get this mfer to work!
+    else:
+        return jsonify({'Login unsuccessful': 'Please try again.'})
 
     
 # @api.route('/<int:pid')
